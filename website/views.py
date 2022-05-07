53426068d2b13cd38f0d925d6b5d1502541115dc
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2022-05-08 03:08:19 +0530

    handle requests
    update pages

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from musicapy.saavn_api.api import SaavnAPI
+from pprint import pprint
 
 api = SaavnAPI()
 
@@ -10,9 +11,32 @@ def index(request):
 
     if request.method == 'POST':
         query = request.POST.get('search_all', None)
-        if query:
+
+        data = None
+        if query and 'saavn' in query:
+            if 'song' in query:
+                identifier = api.create_identifier(query, 'song')
+                data = api.get_song_details(identifier)
+
+                pprint(data)
+                songs = []
+                for song in data.get('songs', []):
+                    song_data = {"title": song.get('song', None), "image": song.get('image', None), "year": song.get(
+                        'year', None), "subtitle": song.get('primary_artists', None), "perma_url": song.get('perma_url', None), "type": "song"}
+                    songs.append(song_data)
+                data = {"results": songs}
+
+            elif 'album' in query:
+                identifier = api.create_identifier(query, 'album')
+                data = api.get_album_details(identifier)
+
+                # format album
+                data = {"results": [{"title": data.get('title', None), "image": data.get(
+                    'image', None), "year": data.get('year', None), "subtitle": data.get('primary_artists', None), "perma_url": data.get('perma_url', None), "type": "album"}]}
+        elif query:
             data = api.search_all(str(query))
-            context["search_result"] = data
+
+        context["search_result"] = data
 
     return render(request, 'index.html', context=context)
 
@@ -25,6 +49,9 @@ def songs_page(request):
         if song_query:
             data = api.search_song(song_query)
             context['search_result'] = data
+    # elif request.method == 'GET':
+        # data = api.get_trending()
+        # context['search_result'] = {'results' : data}
 
     return render(request, 'songs.html', context=context)
 

9eaa8f1e0ab22d801952a07525929b2cabe7aa9e
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2022-05-07 23:23:05 +0530

    implement search function
    create cards for songs page

--- website/views.py
+++ website/views.py
@@ -6,23 +6,36 @@ api = SaavnAPI()
 
 def index(request):
     context = {"title": "HomePage", "search_for": "Song/Album Name/Saavn Link",
-                   "active_page": "home", "search_method": "search_all"}
-
+               "active_page": "home", "search_method": "search_all"}
 
     if request.method == 'POST':
         query = request.POST.get('search_all', None)
         if query:
             data = api.search_all(str(query))
-            print(data)
             context["search_result"] = data
 
-        
     return render(request, 'index.html', context=context)
 
 
 def songs_page(request):
-    return render(request, 'songs.html', context={"title": "Songs", "search_for": "Song Name/Saavn Link", "active_page": "songs", "search_method": "search_songs"})
+    context = {"title": "Songs", "search_for": "Song Name/Saavn Link",
+               "active_page": "songs", "search_method": "search_songs"}
+    if request.method == 'POST':
+        song_query = request.POST.get('search_songs', None)
+        if song_query:
+            data = api.search_song(song_query)
+            context['search_result'] = data
+
+    return render(request, 'songs.html', context=context)
 
 
 def albums_page(request):
-    return render(request, 'albums.html', context={"title": "Albums", "search_for": "Album Name/Saavn Link", "active_page": "albums", "search_method": "search_albums"})
+    context = {"title": "Albums", "search_for": "Album Name/Saavn Link",
+               "active_page": "albums", "search_method": "search_albums"}
+    if request.method == 'POST':
+        album_query = request.POST.get('search_albums', None)
+        if album_query:
+            data = api.search_album(album_query)
+            context['search_result'] = data
+
+    return render(request, 'albums.html', context=context)

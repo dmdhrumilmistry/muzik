1de2db5cc371968dd1b1773025db5f716eafd606
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2022-05-09 12:14:58 +0530

    fix project settings

--- muzik/settings.py
+++ muzik/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-if DEBUG == False:
+if DEBUG != False:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
     ]
